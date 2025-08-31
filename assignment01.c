cfcca760d23e75c16eb465f250ab0eb2796adabb
aswab007 <bsai23020@itu.edu.pk>
2025-09-01 00:34:29 +0500

    Added child loop

--- assignment01.c
+++ assignment01.c
@@ -1,20 +1,24 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
+#include <stdio.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
 
-int main() {
-    int pipe1[2], pipe2[2];  // seting up pipess
-    pipe(pipe1);
-    pipe(pipe2);
+int main() { 
+    int pipe1[2], pipe2[2]; // pipes
+    pipe(pipe1); 
+    pipe(pipe2); 
 
-    pid_t pid = fork(); // creating child,ig this ll work
+    pid_t pid = fork(); // child
 
-    if (pid == 0) {
-        // child part gonna come here
+    if (pid == 0) { 
+        close(pipe1[1]); // child will not write here 
+        close(pipe2[0]); // child will not read from here
 
-    } else {
-        // parent part gonna come here
+        int message; 
+        for (int i = 0; i < 1000000; i++) { 
+            read(pipe1[0], &message);  // reading number from parent         // will handle this error subha
+            write(pipe2[1], &message);  // sending number back to parent
+        } 
+    } 
 
-    return 0;
-}
-
+    return 0; 
+} 
