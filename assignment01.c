bd3ce7cd740ce5dc6a807fce7759a3c480afee62
aswab007 <bsai23020@itu.edu.pk>
2025-09-01 00:23:11 +0500

    Added Child/Parent

--- assignment01.c
+++ assignment01.c
@@ -3,12 +3,18 @@
 #include <sys/types.h>
 
 int main() {
-    int pipe1[2], pipe2[2];  // setting up the pipess
+    int pipe1[2], pipe2[2];  // seting up pipess
     pipe(pipe1);
     pipe(pipe2);
 
-    pid_t pid = fork();  // creating child,ig this is gonna work
+    pid_t pid = fork(); // creating child,ig this ll work
 
+    if (pid == 0) {
+        // child part gonna come here
+
+    } else {
+        // parent part gonna come here
 
     return 0;
 }
+
