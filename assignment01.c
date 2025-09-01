b4b84a00aaa31d6e54273f865c1591baefa50d82
aswab007 <bsai23020@itu.edu.pk>
2025-09-01 10:42:25 +0500

    Getting Timing & closing pipes

--- assignment01.c
+++ assignment01.c
@@ -1,33 +1,48 @@
-#include <stdio.h> 
-#include <unistd.h> 
-#include <sys/types.h> 
- 
-int main() { 
-    int pipe1[2], pipe2[2]; 
-    pipe(pipe1); 
-    pipe(pipe2); 
- 
-    pid_t pid = fork(); // child
- 
-    if (pid == 0) { 
-        close(pipe1[1]); 
-        close(pipe2[0]); 
- 
-        int message; 
-        for (int i = 0; i < 1000000; i++) { 
-            read(pipe1[0], &message, sizeof(int)); // child read number 
-            write(pipe2[1], &message, sizeof(int)); // child sends the number back
-        } 
-    } else { 
-        close(pipe1[0]); 
-        close(pipe2[1]); 
-        int message; 
-        for (int i = 0; i < 1000000; i++) { 
-            message = i; 
-            write(pipe1[1], &message, sizeof(int)); 
-            read(pipe2[0], &message, sizeof(int));  
-        } 
-    } 
- 
-    return 0; 
-} 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+
+int main() {
+    int pipe1[2], pipe2[2];
+    pipe(pipe1);
+    pipe(pipe2);
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        close(pipe1[1]);
+        close(pipe2[0]);
+
+        int message;
+        for (int i = 0; i < 1000000; i++) {
+            read(pipe1[0], &message, sizeof(int));
+            write(pipe2[1], &message, sizeof(int));
+        }
+
+        close(pipe1[0]);
+        close(pipe2[1]);
+    } else {
+        close(pipe1[0]);
+        close(pipe2[1]);
+
+        struct timeval start, end;
+        gettimeofday(&start, NULL);
+
+        int message;
+        for (int i = 0; i < 1000000; i++) {
+            message = i;
+            write(pipe1[1], &message, sizeof(int));
+            read(pipe2[0], &message, sizeof(int));
+        }
+
+        gettimeofday(&end, NULL);
+
+        close(pipe1[1]);
+        close(pipe2[0]);
+        wait(NULL);
+    }
+
+    return 0;
+}
