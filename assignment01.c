339765074373f149597d75435268706c037d326d
aswab007 <bsai23020@itu.edu.pk>
2025-09-01 10:15:35 +0500

    Fixed child loop and added parent loop

--- assignment01.c
+++ assignment01.c
@@ -1,24 +1,33 @@
 #include <stdio.h> 
 #include <unistd.h> 
 #include <sys/types.h> 
-
+ 
 int main() { 
-    int pipe1[2], pipe2[2]; // pipes
+    int pipe1[2], pipe2[2]; 
     pipe(pipe1); 
     pipe(pipe2); 
-
+ 
     pid_t pid = fork(); // child
-
+ 
     if (pid == 0) { 
-        close(pipe1[1]); // child will not write here 
-        close(pipe2[0]); // child will not read from here
-
+        close(pipe1[1]); 
+        close(pipe2[0]); 
+ 
         int message; 
         for (int i = 0; i < 1000000; i++) { 
-            read(pipe1[0], &message);  // reading number from parent         // will handle this error subha
-            write(pipe2[1], &message);  // sending number back to parent
+            read(pipe1[0], &message, sizeof(int)); // child read number 
+            write(pipe2[1], &message, sizeof(int)); // child sends the number back
+        } 
+    } else { 
+        close(pipe1[0]); 
+        close(pipe2[1]); 
+        int message; 
+        for (int i = 0; i < 1000000; i++) { 
+            message = i; 
+            write(pipe1[1], &message, sizeof(int)); 
+            read(pipe2[0], &message, sizeof(int));  
         } 
     } 
-
+ 
     return 0; 
 } 
